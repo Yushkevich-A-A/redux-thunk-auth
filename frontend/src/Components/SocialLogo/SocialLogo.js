9e93cdbf6c3aed0c535a54e064b39626424d78fc
Yushkevich-A-A <Mortifer1993@yandex.ru>
2021-10-28 23:27:20 +0300

    edit profile reducer

--- frontend/src/Components/SocialLogo/SocialLogo.js
+++ frontend/src/Components/SocialLogo/SocialLogo.js
@@ -5,7 +5,7 @@ function SocialLogo() {
     return (
         <div className='social-logo'>
             <h1 className='social-logo-h1'>Neto Social</h1>
-            <p className='social-logo-desc'>Faceboock and VK killer</p>
+            <p className='social-logo-desc'>Facebook and VK killer</p>
         </div>
     )
 }
